5cb1c94986d700713247ca796232e88da617d292
Johan <johlits@palplanner.com>
2020-12-19 15:52:10 +0100

    day 10 to 19

---
+++ Advent/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Advent
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("day 19");
+            var advent = new Day19();
+            advent.Run();
+
+        }
+    }
+}
